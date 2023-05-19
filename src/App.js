54b25adc58aec122aa128c0523e76f87655b22a2
Ayaan <ayaan@metasky.ai>
2023-05-19 17:05:51 +0530

    refactor(*): refactored entire codebase

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Home from "./JavaScript/Home";
+import NavBar from "./JavaScript/NavBar";
+import Send from "./JavaScript/Send";
+import Receive from "./JavaScript/Recieve";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/send" element={<Send />} />
+          <Route path="/download" element={<Receive />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/JavaScript/NavBar.js
@@ -0,0 +1,42 @@
+import "../Css/NavBar.css";
+import React from "react";
+import { Navbar, Nav, NavItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const NavBar = (props) => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen] = useState(false);
+
+  // const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div className="Navbar">
+      <Navbar color="transparent" light expand="md">
+        {/*onClick={toggle} */}
+        <Link to="/">
+          <img
+            src={require("./image/logo.png")}
+            className="Brand"
+            width="60px"
+            alt="logo of SwiftShare website"
+          />
+        </Link>
+        {/* <Collapse isOpen={isOpen} navbar> */}
+        <Nav className="me-auto" navbar>
+          <NavItem className="Nav-item">
+            <Link to="/send" style={{ textDecoration: "none" }}>
+              <p className="Nav-elements">Send</p>
+            </Link>
+          </NavItem>
+          <NavItem className="Nav-item">
+            <Link to="/download" style={{ textDecoration: "none" }}>
+              <p className="Nav-elements">Download</p>
+            </Link>
+          </NavItem>
+        </Nav>
+        {/* </Collapse> */}
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
